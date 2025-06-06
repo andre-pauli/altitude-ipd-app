83e9df97f6fbdf4d1a0c44a2f0514c1ac19c2ee0
André Pauli <andre.pauli@granter.com.br>
2025-06-06 07:42:10 -0300

    feat: adiciono o home e remove o kiosk mode

--- android/app/src/main/kotlin/com/example/altitude_ipd_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/altitude_ipd_app/MainActivity.kt
@@ -29,7 +29,6 @@ class MainActivity : FlutterActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableKioskMode() // Habilita o modo quiosque ao criar a atividade
     }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
@@ -123,32 +122,6 @@ class MainActivity : FlutterActivity() {
 
     override fun onResume() {
         super.onResume()
-        enableKioskMode() // Reativa o modo quiosque quando a atividade volta ao foco
-    }
-
-    private fun enableKioskMode() {
-        val decorView = window.decorView
-        decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        or View.SYSTEM_UI_FLAG_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                )
-
-        decorView.setOnSystemUiVisibilityChangeListener { visibility ->
-            if ((visibility and View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-                decorView.systemUiVisibility = (
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        )
-            }
-        }
     }
 
     private fun isValidJson(json: String): Boolean {
